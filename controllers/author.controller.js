20dd73570ccb0c5ce23901f871e9052c9498a742
Jonathan <jnolascob@gmail.com>
2017-12-21 15:53:33 -0500

    Clean code

--- controllers/author.controller.js
+++ controllers/author.controller.js
@@ -1,25 +1,25 @@
 const Author = require('../models/author.model');
 
 exports.author_create = (req, res) => {
-    let author = new Author({
-        username: req.body.username
-    });
+  const author = new Author({
+    username: req.body.username,
+  });
 
-    author.save()
-    .then(item => {
-        res.status(200).send({message: 'success', data: item});
+  author.save()
+    .then((item) => {
+      res.status(200).send({ message: 'success', data: item });
     })
-    .catch(err => {
-        res.status(400).send({message: 'error', data: ''});
+    .catch((err) => {
+      res.status(400).send({ message: 'error', data: err });
     });
 };
 
 exports.author_list = (req, res) => {
-    Author.find({})
-    .then(authors => {
-        res.status(200).send({message: 'success', data: authors});
+  Author.find({})
+    .then((authors) => {
+      res.status(200).send({ message: 'success', data: authors });
     })
-    .catch(err => {
-        res.status(400).send({message: 'error', data: ''});
+    .catch((err) => {
+      res.status(400).send({ message: 'error', data: err });
     });
 };
--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,25 +1,25 @@
-let Category = require('../models/category.model');
+const Category = require('../models/category.model');
 
 exports.category_create = (req, res) => {
-    let category = new Category({
-        name: req.body.name
-    });
+  const category = new Category({
+    name: req.body.name,
+  });
 
-    category.save()
-    .then(item => {
-        res.status(200).send({message: 'success', data: item});
+  category.save()
+    .then((item) => {
+      res.status(200).send({ message: 'success', data: item });
     })
-    .catch(err => {
-        res.status(400).send({message: 'error', data: ''});
+    .catch((err) => {
+      res.status(400).send({ message: 'error', data: err });
     });
 };
 
 exports.category_list = (req, res) => {
-    Category.find({})
-    .then(categories => {
-        res.status(200).send({message: 'success', data: categories});
+  Category.find({})
+    .then((categories) => {
+      res.status(200).send({ message: 'success', data: categories });
     })
-    .catch(err => {
-        res.status(400).send({message: 'error', data: ''});
+    .catch((err) => {
+      res.status(400).send({ message: 'error', data: err });
     });
 };
